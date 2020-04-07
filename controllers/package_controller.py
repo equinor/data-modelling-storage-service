38e47b49665c0d25ac70b46216fc14a1b5f66b9a
Eirik Ola Aksnes <eoaksnes@gmail.com>
2020-04-07 09:18:22 +0200

    update spec

--- controllers/package_controller.py
+++ controllers/package_controller.py
@@ -1,3 +1,6 @@
+from api.classes.dto import DTO
+
+from api.core.repository.repository_exceptions import FileNotFoundException
 from flask import Response
 
 from api.core.repository.repository_factory import get_repository
@@ -9,4 +12,14 @@ import json
 def get(data_source_id):
     document_service = DocumentService(repository_provider=get_repository)
     root_packages = document_service.get_root_packages(data_source_id=data_source_id)
-    return Response(json.dumps([package.to_dict() for package in root_packages]), mimetype="application/json", status=200)
+    return Response(json.dumps([package.to_dict() for package in root_packages]), mimetype="application/json",
+                    status=200)
+
+
+def find_by_name(data_source_id, name):
+    package: DTO = get_repository(data_source_id).find(
+        {"type": "system/DMT/Package", "isRoot": True, "name": name}, single=True
+    )
+    if not package:
+        raise FileNotFoundException(data_source_id, name, is_root=True)
+    return package.to_dict()["data"]
