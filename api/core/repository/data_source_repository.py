5f781aaacd7d8ff5b7272cb5f468a7bb84a7970f
Eirik Ola Aksnes <eoaksnes@gmail.com>
2020-04-16 11:40:22 +0200

    fix

--- api/core/repository/data_source_repository.py
+++ api/core/repository/data_source_repository.py
@@ -13,30 +13,30 @@ class DataSourceRepository:
             {"id": "local", "host": "client", "name": "Local workspace", "type": DataSourceType.LOCAL.value}
         ]
         for data_source in self.collection.find(
-            filter={"documentType": {"$regex": document_type.value}}, projection=["host", "name", "type"]
+            filter={"documentType": {"$regex": document_type.value}}, projection=["name", "host", "type"]
         ):
             data_source["id"] = data_source.pop("_id")
-            all_sources.append(
-                {
-                    "id": data_source["id"],
-                    "host": data_source["host"],
-                    "name": data_source["name"],
-                    "type": DataSourceType(data_source["type"]).value,
-                }
-            )
+
+            data_source_type = DataSourceType(data_source["type"])
+
+            if data_source_type == DataSourceType.MONGO:
+                all_sources.append(
+                    {
+                        "id": data_source["id"],
+                        "host": data_source["host"],
+                        "name": data_source["name"],
+                        "type": data_source_type.value,
+                    }
+                )
+            if data_source_type == DataSourceType.AZURE_BLOB_STORAGE:
+                # TODO: Remove host?
+                all_sources.append(
+                    {"id": data_source["id"], "name": data_source["name"], "type": data_source_type.value, "host": ""}
+                )
+
         return all_sources
 
     def create(self, id: str, document):
         document["_id"] = id
         result = self.collection.insert_one(document)
         return str(result.inserted_id)
-
-    # def update(self, id: str):
-    #     document = request.get_json()
-    #     validate_mongo_data_source(document)
-    #     result = self.collection.replace_one({"_id": id}, document, upsert=True)
-    #     return str(result.acknowledged)
-    #
-    # def delete(self, id: str):
-    #     result = self.collection.delete_one(filter={"_id": id})
-    #     return result.acknowledged
