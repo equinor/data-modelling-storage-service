722b808570eaca9392aa4fb5873e570fddeb2a2b
Eirik Ola Aksnes <eoaksnes@gmail.com>
2020-04-20 09:40:29 +0200

    fix

--- api/core/serializers/get_data_source_serializer.py
+++ api/core/serializers/get_data_source_serializer.py
@@ -8,8 +8,9 @@ class GetDataSourceSerializer(json.JSONEncoder):
             to_serialize = {
                 "id": data_source["_id"],
                 "name": data_source["name"],
-                "host": data_source["host"],
+                "host": data_source.get("host", ""),
                 "type": data_source["type"],
+                "documentType": data_source["documentType"],
             }
             return to_serialize
         except AttributeError:
