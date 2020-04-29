5d6195fc3d20ed48b00d007506b3145ce31d70a7
Eirik Ola Aksnes <eoaksnes@gmail.com>
2020-04-29 11:41:14 +0200

    specify env

--- api/init.sh
+++ api/init.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 set -euo pipefail
 
-if [[ ! -v $ENVIRONMENT ]]; then
-  if [ ! -e /usr/src/app/api/home/first-run-false ] && [ "$ENVIRONMENT" = 'local' ]; then
-    echo "Importing data"
-    /usr/src/app/api/reset-database.sh
-    touch /usr/src/app/api/home/first-run-false
-  fi
+
+if [ ! -e /usr/src/app/api/home/first-run-false ] && [ "$ENVIRONMENT" = 'local' ]; then
+  echo "Importing data"
+  /usr/src/app/api/reset-database.sh
+  touch /usr/src/app/api/home/first-run-false
 fi
 
 flask run --host=0.0.0.0
