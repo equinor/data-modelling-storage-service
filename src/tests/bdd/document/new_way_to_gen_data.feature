3dcee82715802cea967323b0f31a4b909eb2d5cc
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2021-04-27 11:31:51 +0200

    update variable names and add functionality creating ui recipies

--- src/tests/bdd/document/new_way_to_gen_data.feature
+++ src/tests/bdd/document/new_way_to_gen_data.feature
@@ -18,61 +18,76 @@ Feature: # Enter feature name here
       | system           | db   | 27017 | maf      | maf      | false | system     | local    | system     | mongo-db | default      |
 
 
-    Given Add to data source "test-source-name"
-    """
-    RootPackages:
-      TestData:
-        id: 1
-        content:
-          itemType:
-            id: 2
-            type: system/SIMOS/Blueprint
-            attributes:
-              list:
-                attributeType: string
-                optional: true
-              extra:
-                attributeType: string
-              complexList:
-                dimensions: "*"
-                optional: true
-                attributeType: test-source-name/TestData/ItemTypeTwo
-          ItemTypeTwo:
-            id: 3
-            type: system/SIMOS/Blueprint
-            attributes:
-              extra:
-                attributeType: string
-                optional: true
-          TestContainer:
-            id: 6
-            type: system/SIMOS/Blueprint
-            attributes:
-              itemContained:
-                optional: true
-                attributeType: test-source-name/TestData/ItemType
-              itemsContained:
-                optional: true
-                dimensions: "*"
-                attributeType: test-source-name/TestData/ItemType
-              itemNotContained:
-                optional: false
-                attributeType: test-source-name/TestData/ItemType
-              itemsNotContained:
-                optional: true,
-                dimensions: "*"
-                attributeType: test-source-name/TestData/ItemType
-
-      TestData2:
-        id: 4
-        content:
-          ItemType3:
-            id: 5
-            type: system/SIMOS/Blueprint
-            attributes:
-              power:
-                attributeType: int
-    """
-
-
-  Scenario: # Enter scenario name here
+    Scenario: create data
+      Given Add to data source "test-source-name"
+      """
+      RootPackages:
+        TestData:
+          id: 1
+          content:
+            itemType:
+              id: 2
+              type: system/SIMOS/Blueprint
+              attributes:
+                list:
+                  attributeType: string
+                  optional: true
+                extra:
+                  attributeType: string
+                complexList:
+                  dimensions: "*"
+                  optional: true
+                  attributeType: test-source-name/TestData/ItemTypeTwo
+            ItemTypeTwo:
+              id: 3
+              type: system/SIMOS/Blueprint
+              attributes:
+                extra:
+                  attributeType: string
+                  optional: true
+            TestContainer:
+              id: 6
+              type: system/SIMOS/Blueprint
+              attributes:
+                itemContained:
+                  optional: true
+                  attributeType: test-source-name/TestData/ItemType
+                itemsContained:
+                  optional: true
+                  dimensions: "*"
+                  attributeType: test-source-name/TestData/ItemType
+                itemNotContained:
+                  optional: false
+                  attributeType: test-source-name/TestData/ItemType
+                itemsNotContained:
+                  optional: true,
+                  dimensions: "*"
+                  attributeType: test-source-name/TestData/ItemType
+              storageRecipes:
+                DefaultStorageRecipe:
+                  type: system/SIMOS/StorageRecipe
+                  attributes:
+                    itemNotContained:
+                      type: test-source-name/TestData/ItemType
+                      contained: false
+                      storageTypeAffinity: blob
+                    itemsNotContained:
+                      type: test-source-name/TestData/ItemType
+                      contained: false
+                ExtraStorageRecipie:
+                  type: system/SIMOS/StorageRecipe
+                  attributes:
+                    itemNotContained:
+                      type: test-source-name/TestData/ItemType
+                      contained: false
+                      storageTypeAffinity: azure
+        TestData2:
+          id: 4
+          content:
+            ItemType3:
+              id: 5
+              type: system/SIMOS/Blueprint
+              attributes:
+                power:
+                  attributeType: int
+      """
--- src/tests/bdd/steps/schemas.py
+++ src/tests/bdd/steps/schemas.py
@@ -28,107 +28,127 @@ def step_impl_2(context, uid: str, data_source_id: str):
 
 
 
-def store_document_in_data_source(uid: str, dataSourceId: str, document: Dict):
+def store_document_in_data_source(uid: str, data_source_id: str, document: Dict):
     document: DTO = DTO(uid=uid, data=document)
-    documentRepository = get_data_source(dataSourceId)
+    documentRepository = get_data_source(data_source_id)
     documentRepository.add(document)
 
 
-@given('Add to data source "{dataSourceId}"')
-def convert_yaml_to_json(context, dataSourceId: str):
+@given('Add to data source "{data_source_id}"')
+def convert_yaml_to_json(context, data_source_id: str):
     yaml_content_as_dict: dict = yaml.safe_load(context.text)
-    packageNames: List[string] = list(yaml_content_as_dict['RootPackages'].keys()) #['TestData', 'TestData2']
+    package_names: List[str] = list(yaml_content_as_dict['RootPackages'].keys())
     package_blueprint_info: Dict
 
     # generate package and blueprint documents and store them in data source
-    for packageName in packageNames:
-        packageBlueprints: List[Dict] = yaml_content_as_dict['RootPackages'][f"{packageName}"]['content']
-        packageContent: List[Dict] = generate_package_content(packageName, packageBlueprints, dataSourceId)
-        packageDocument: Dict = generate_root_package_document(packageName, packageContent)
+    for package_name in package_names:
+        package_blueprints: List[Dict] = yaml_content_as_dict['RootPackages'][f"{package_name}"]['content']
+        package_content: List[Dict] = generate_package_content(package_name, package_blueprints, data_source_id)
+        package_document: Dict = generate_root_package_document(package_name, package_content)
 
-        packageUid = yaml_content_as_dict['RootPackages'][f"{packageName}"]['id']
-        store_document_in_data_source(packageUid, dataSourceId, packageDocument)
+        package_uid = yaml_content_as_dict['RootPackages'][f"{package_name}"]['id']
+        store_document_in_data_source(package_uid, data_source_id, package_document)
         print("--- PACKAGE DOCUMENT ---")
-        print(json.dumps(packageDocument, indent=2))
+        print(json.dumps(package_document, indent=2))
         print(" ")
 
         #store blueprint documents in data source
-        for blueprintName in list(packageBlueprints.keys()):
-            blueprint: Dict = packageBlueprints[f'{blueprintName}']
-            blueprintDocument: Dict = generate_blueprint_document(blueprint, blueprintName)
-            blueprintUid = blueprint['id']
-            store_document_in_data_source(blueprintUid, dataSourceId, blueprintDocument)
-            print("--- BLUEPRINT DOCUMNET ---")
-            print(json.dumps(blueprintDocument, indent=2))
+        for blueprint_name in list(package_blueprints.keys()):
+            blueprint: Dict = package_blueprints[f'{blueprint_name}']
+            blueprint_document: Dict = generate_blueprint_document(blueprint, blueprint_name)
+            blueprint_uid = blueprint['id']
+            store_document_in_data_source(blueprint_uid, data_source_id, blueprint_document)
+            print("--- BLUEPRINT DOCUMENT ---")
+            print(json.dumps(blueprint_document, indent=2))
             print(" ")
 
 
 
-def generate_blueprint_document(blueprint: Dict, blueprintName: str, storageRecipes: List = []):
-    attributesList: List[Dict] = []
-    append_default_attributes(
-        attributesList)  # default attributes = attributes that exist for all blueprints: type, description and name
+def generate_blueprint_document(blueprint: Dict, blueprint_name: str):
+    attributes_list: List[Dict] = []
+    append_default_attributes(attributes_list)  # default attributes = type, description and name
 
-    for attributeName in list(blueprint['attributes'].keys()):
-        attribute = blueprint['attributes'][f'{attributeName}']
-        attribute['name'] = attributeName
-        attribute['type'] = "system/SIMOS/BlueprintAttribute" # is it ok to hardcode this value??
-        attributesList.append(attribute)
+    for attribute_name in list(blueprint['attributes'].keys()):
+        attribute = blueprint['attributes'][f'{attribute_name}']
+        attribute['name'] = attribute_name
+        attribute['type'] = "system/SIMOS/BlueprintAttribute"
+        attributes_list.append(attribute)
 
+    storage_recipes: List = get_storage_recipes(blueprint)
     blueprint_document = {
         "type": blueprint['type'],
-        "name": blueprintName,
+        "name": blueprint_name,
         "description": "",
-        "attributes" : attributesList,
-        "storageRecipes": storageRecipes,
+        "attributes" : attributes_list,
+        "storageRecipes": storage_recipes,
         "uiRecipes": []
     }
     return blueprint_document
 
-def append_default_attributes(attributesList: List[Dict]):
-    attributesList.append(
+
+def get_storage_recipes(blueprint: Dict):
+    if "storageRecipes" in blueprint:
+        storage_recipes: List[Dict] = []
+        for storage_recipe_name in list(blueprint['storageRecipes'].keys()):
+            recipe: Dict = blueprint['storageRecipes'][f'{storage_recipe_name}']
+            recipie_attributes_list: list = []
+            for attribute_name in recipe['attributes']:
+                pass
+                recipe_attribute: Dict = recipe['attributes'][f'{attribute_name}']
+                recipe_attribute['name'] = attribute_name
+                recipie_attributes_list.append(recipe_attribute)
+            recipe['attributes'] = recipie_attributes_list
+            recipe['name'] = storage_recipe_name
+            recipe['description'] = ""
+            storage_recipes.append(recipe)
+    else:
+        storage_recipes: List = []
+    return storage_recipes
+
+
+def append_default_attributes(attributes_list: List[Dict]):
+    attributes_list.append(
         {
             "attributeType": "string",
             "type": "system/SIMOS/BlueprintAttribute",
             "name": "name"
         }
     )
-    attributesList.append({
+    attributes_list.append({
             "attributeType": "string",
             "type": "system/SIMOS/BlueprintAttribute",
             "optional": "true",
             "default": "",
             "name": "description"
         })
-    attributesList.append({
+    attributes_list.append({
             "attributeType": "string",
             "type": "system/SIMOS/BlueprintAttribute",
             "name": "type"
         })
 
 
-def generate_package_content(packageName: str, packageBlueprints: List[Dict], dataSourceId: str):
-    ###### packageBlueprintNames: list[str] = list(packageBlueprints.keys())
-    packageContent: List[Dict] = []
-    for blueprintName in list(packageBlueprints.keys()):
-        blueprint = packageBlueprints[f"{blueprintName}"]
-        packageBlueprintInfo = {
+def generate_package_content(package_name: str, package_blueprints: List[Dict], data_source_id: str):
+    package_content: List[Dict] = []
+    for blueprint_name in list(package_blueprints.keys()):
+        blueprint = package_blueprints[f"{blueprint_name}"]
+        package_blueprint_info = {
             "_id": blueprint['id'],
-            "name": blueprintName,
-            "type": f"{dataSourceId}/{packageName}/{blueprintName}"
+            "name": blueprint_name,
+            "type": f"{data_source_id}/{package_name}/{blueprint_name}"
         }
-        packageContent.append(packageBlueprintInfo)
-    return packageContent
+        package_content.append(package_blueprint_info)
+    return package_content
 
 #todo move this func to utils or soemthing
-def generate_root_package_document(packageName: str, packageContent: List[Dict], storageRecipes: List = []):
+def generate_root_package_document(package_name: str, package_content: List[Dict], storage_recipes: List = []):
     package: Dict = {
-        "name": packageName,
+        "name": package_name,
         "description": "",
         "type": "system/SIMOS/Package",
-        "content": packageContent,
+        "content": package_content,
         "isRoot": "true",
-        "storageRecipes": storageRecipes,
+        "storageRecipes": storage_recipes,
         "uiRecipes": []
     }
     return package
@@ -136,10 +156,10 @@ def generate_root_package_document(packageName: str, packageContent: List[Dict],
 
 
 @given('there exists a package "{packageName}" with id "{uid}" in data source "{dataSourceId}"')
-def create_package(context, packageName: str, dataSourceId: str, uid: str):
+def create_package(context, package_name: str, data_source_id: str, uid: str):
     package: Dict[Dict, str, str] = {
         "jsonContent": {
-            "name": packageName,
+            "name": package_name,
             "description": "Example package description",
             "type": "system/SIMOS/Package",
             "content": [],
@@ -147,7 +167,7 @@ def create_package(context, packageName: str, dataSourceId: str, uid: str):
             "storageRecipes": [],
             "uiRecipes": [],
         },
-        "dataSourceId": dataSourceId,
+        "dataSourceId": data_source_id,
         "packageId": uid,
     }
 
