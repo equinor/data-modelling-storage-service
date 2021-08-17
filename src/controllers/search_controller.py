0fdac12759e0d43e54b7461e4b2fbc9e3b64a849
Morten Amundsen <MOAMU@equinor.com>
2021-08-17 12:35:45 +0200

    update variable name passed to SearchRequest

--- src/controllers/search_controller.py
+++ src/controllers/search_controller.py
@@ -11,6 +11,6 @@ router = APIRouter()
 def search(data_source_id: str, request: dict, sort_by_attribute: str = "name"):
     use_case = SearchUseCase()
     response = use_case.execute(
-        SearchRequest(data_source_id=data_source_id, data=request, sort_by_attribute=sort_by_attribute)
+        SearchRequest(data_source_id=data_source_id, data=request, dotted_attribute_path=sort_by_attribute)
     )
     return JSONResponse(response.value, status_code=STATUS_CODES[response.type])
