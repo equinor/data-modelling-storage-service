b07020c95ee1d9eace72b03622b2ec9424862567
Stig Ofstad <stigofstad@gmail.com>
2021-11-23 10:25:31 +0100

    run production env with multiple workers managed by gunicorn

--- src/init.sh
+++ src/init.sh
@@ -2,7 +2,12 @@
 set -eu
 
 if [ "$1" = 'api' ]; then
-  python3 /code/app.py run
+  if [ "${ENVIRONMENT:-'local'}" != "local" ]; then
+    cat version.txt || true
+    gunicorn app:create_app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
+  else
+    python3 /code/app.py run
+  fi
 elif [ "$1" = 'reset-app' ]; then
   python3 /code/app.py reset-app
 elif [ "$1" = 'create-key' ]; then
