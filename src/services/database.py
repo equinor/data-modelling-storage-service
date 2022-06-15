b03081889175e8f2986ace56850c142daac77b2d
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2022-06-15 15:19:17 +0200

    reset to make prod env work

--- src/services/database.py
+++ src/services/database.py
@@ -1,16 +1,11 @@
 from pymongo import MongoClient
 from config import config
 
-tls_args = {
-    "tls": True if config.MONGO_SELF_SIGN_CA_CRT else False,
-    "tlsCAFile": config.MONGO_SELF_SIGN_CA_PATH if config.MONGO_SELF_SIGN_CA_CRT else None,
-}
-
 mongo_client = None
 if config.MONGO_URI:
-    mongo_client = MongoClient(config.MONGO_URI, connect=False, **tls_args)
+    mongo_client = MongoClient(config.MONGO_URI, connect=False)
 elif config.MONGO_USERNAME and config.MONGO_PASSWORD:
-    mongo_client = MongoClient("db", username=config.MONGO_USERNAME, password=config.MONGO_PASSWORD, **tls_args)
+    mongo_client = MongoClient("db", username=config.MONGO_USERNAME, password=config.MONGO_PASSWORD)
 
 else:
     raise ValueError("Missing credentials for the DataSource database")
