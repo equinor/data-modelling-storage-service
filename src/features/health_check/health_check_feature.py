97d28cfd849018ebaed8160a7cf9d12dd46911ab
Morten Amundsen <MOAMU@equinor.com>
2022-08-03 13:45:46 +0200

    refactor to follow template-fastapi-react

--- src/features/health_check/health_check_feature.py
+++ src/features/health_check/health_check_feature.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.responses import PlainTextResponse
 
-router = APIRouter()
+router = APIRouter(tags=["health_check"], prefix="/healthcheck")
 
 
-@router.get("/healthcheck", operation_id="healthcheck")
-async def healthcheck():
+@router.get(
+    "",
+    status_code=status.HTTP_200_OK,
+    responses={status.HTTP_200_OK: {"model": str, "content": {"text/plain": {"example": "OK"}}}},
+    response_class=PlainTextResponse,
+)
+async def get():
     return "OK"
