2ea5b8323e1f42646674756ce23f8cca3abc6ddb
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2022-08-09 15:35:40 +0200

    blob use cases

--- src/features/blob/blob_feature.py
+++ src/features/blob/blob_feature.py
@@ -5,8 +5,8 @@ from common.responses import create_response
 
 from authentication.authentication import auth_w_jwt_or_pat
 from authentication.models import User
-from .use_cases.get_blob_use_case import GetBlobRequest, GetBlobUseCase
-from .use_cases.put_blob_use_case import PutBlobRequest, PutBlobUseCase
+from .use_cases.get_blob_use_case import get_blob_use_case
+from .use_cases.upload_blob_use_case import upload_blob_use_case
 
 router = APIRouter(tags=["default", "blob"], prefix="/blobs")
 
@@ -22,12 +22,10 @@ responses = {200: {"content": {"application/octet-stream": {"schema": {"type": "
 )
 @create_response(PlainTextResponse)
 def get_by_id(data_source_id: str, blob_id: str, user: User = Depends(auth_w_jwt_or_pat)):
-    use_case = GetBlobUseCase(user)
-    return use_case.execute(GetBlobRequest(data_source_id=data_source_id, blob_id=blob_id))
+    return get_blob_use_case(user=user, data_source_id=data_source_id, blob_id=blob_id)
 
 
 @router.put("/{data_source_id}/{blob_id}", operation_id="blob_upload", response_model=str)
 @create_response(PlainTextResponse)
 def upload(data_source_id: str, blob_id: str, file: UploadFile = File(...), user: User = Depends(auth_w_jwt_or_pat)):
-    use_case = PutBlobUseCase(user)
-    return use_case.execute(PutBlobRequest(data_source_id=data_source_id, blob_id=blob_id, file=file))
+    return upload_blob_use_case(user=user, data_source_id=data_source_id, blob_id=blob_id, file=file)
