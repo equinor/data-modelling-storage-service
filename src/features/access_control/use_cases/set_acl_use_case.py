0cc58b53b7a3075871fc41bede9225a292ab45d3
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2022-08-09 15:04:37 +0200

    access control use cases

--- src/features/access_control/use_cases/set_acl_use_case.py
+++ src/features/access_control/use_cases/set_acl_use_case.py
@@ -1,23 +1,8 @@
 from authentication.models import ACL, User
-from restful.request_types.shared import DataSource
-
-from restful.use_case import UseCase
 from services.document_service import DocumentService
 
 
-class SetACLRequest(DataSource):
-    document_id: str
-    acl: ACL
-    recursively: bool
-
-
-class SetACLUseCase(UseCase):
-    def __init__(self, user: User):
-        self.user = user
-
-    def process_request(self, req: SetACLRequest):
-        document_service = DocumentService(user=self.user)
-        document_service.set_acl(
-            data_source_id=req.data_source_id, document_id=req.document_id, acl=req.acl, recursively=req.recursively
-        )
-        return "OK"
+def set_acl_use_case(user: User, data_source_id: str, document_id: str, acl: ACL, recursively: bool):
+    document_service = DocumentService(user=user)
+    document_service.set_acl(data_source_id=data_source_id, document_id=document_id, acl=acl, recursively=recursively)
+    return "OK"
--- src/restful/use_case.py
+++
@@ -1,6 +0,0 @@
-class UseCase:
-    def execute(self, request_object=None):
-        return self.process_request(request_object)
-
-    def process_request(self, request_object):
-        raise NotImplementedError("process_request() not implemented by UseCase class")
