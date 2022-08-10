9e6ef2173818f16ab54482f33f63cd5d8931db2b
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2022-08-10 11:12:30 +0200

    reference use cases

--- src/features/reference/use_cases/delete_reference_use_case.py
+++ src/features/reference/use_cases/delete_reference_use_case.py
@@ -1,24 +1,13 @@
 from authentication.models import User
-from restful.request_types.shared import DataSource
-
-from restful.use_case import UseCase
 from services.document_service import DocumentService
 
 
-class DeleteReferenceRequest(DataSource):
-    document_id: str
-    attribute: str
-
-
-class DeleteReferenceUseCase(UseCase):
-    def __init__(self, user: User):
-        self.user = user
-
-    def process_request(self, req: DeleteReferenceRequest):
-        document_service = DocumentService(user=self.user)
-        document = document_service.remove_reference(
-            data_source_id=req.data_source_id,
-            document_id=req.document_id,
-            attribute_path=req.attribute,
-        )
-        return document
+# todo data_source_id has requirements
+def delete_reference_use_case(user: User, data_source_id: str, document_id: str, attribute: str):
+    document_service = DocumentService(user=user)
+    document = document_service.remove_reference(
+        data_source_id=data_source_id,
+        document_id=document_id,
+        attribute_path=attribute,
+    )
+    return document
