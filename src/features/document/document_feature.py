e79699da6419d553bb4262c5a2b6724233211edd
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2022-08-10 09:16:52 +0200

    document use cases

--- src/features/document/document_feature.py
+++ src/features/document/document_feature.py
@@ -8,9 +8,9 @@ from authentication.models import User
 
 from common.responses import create_response
 
-from .use_cases.get_document_by_path_use_case import GetDocumentByPathRequest, GetDocumentByPathUseCase
-from .use_cases.get_document_use_case import GetDocumentRequest, GetDocumentUseCase
-from .use_cases.update_document_use_case import UpdateDocumentRequest, UpdateDocumentUseCase
+from .use_cases.get_document_by_path_use_case import get_document_by_path_use_case
+from .use_cases.get_document_use_case import get_document_use_case
+from .use_cases.update_document_use_case import update_document_use_case
 
 router = APIRouter(tags=["default", "document"], prefix="/documents")
 
@@ -20,7 +20,6 @@ router = APIRouter(tags=["default", "document"], prefix="/documents")
 def get_by_id(
     data_source_id: str,
     document_id: str,
-    ui_recipe: Optional[str] = None,
     attribute: Optional[str] = None,
     depth: conint(gt=-1, lt=1000) = 999,
     user: User = Depends(auth_w_jwt_or_pat),
@@ -32,15 +31,12 @@ def get_by_id(
             "An attribute was specified in both the 'attribute' parameter and the 'document_id' parameter."
             "Please provide a single attribute specification."
         )
-    use_case = GetDocumentUseCase(user)
-    return use_case.execute(
-        GetDocumentRequest(
-            data_source_id=data_source_id,
-            document_id=id_list[0],
-            ui_recipe=ui_recipe,
-            attribute=attribute if len(id_list) == 1 else id_list[1],
-            depth=depth,
-        )
+    return get_document_use_case(
+        user=user,
+        document_id=id_list[0],
+        data_source_id=data_source_id,
+        attribute=attribute if len(id_list) == 1 else id_list[1],
+        depth=depth,
     )
 
 
@@ -52,7 +48,6 @@ def get_by_id(
 @create_response(JSONResponse)
 def get_by_path(
     data_source_id: str,
-    ui_recipe: Optional[str] = None,
     attribute: Optional[str] = None,
     path: Optional[str] = None,
     user: User = Depends(auth_w_jwt_or_pat),
@@ -60,10 +55,7 @@ def get_by_path(
     """
     Get a document by it's path in the form "{dataSource}/{rootPackage}/{subPackage(s)?/{name}
     """
-    use_case = GetDocumentByPathUseCase(user)
-    return use_case.execute(
-        GetDocumentByPathRequest(data_source_id=data_source_id, path=path, ui_recipe=ui_recipe, attribute=attribute)
-    )
+    return get_document_by_path_use_case(user=user, data_source_id=data_source_id, path=path, attribute=attribute)
 
 
 @router.put("/{data_source_id}/{document_id}", operation_id="document_update")
@@ -77,14 +69,13 @@ def update(
     update_uncontained: Optional[bool] = False,
     user: User = Depends(auth_w_jwt_or_pat),
 ):
-    update_use_case = UpdateDocumentUseCase(user)
-    return update_use_case.execute(
-        UpdateDocumentRequest(
-            data_source_id=data_source_id,
-            data=data,
-            document_id=document_id,
-            attribute=attribute,
-            files=files,
-            update_uncontained=update_uncontained,
-        )
+
+    return update_document_use_case(
+        user=user,
+        document_id=document_id,
+        data_source_id=data_source_id,
+        data=data,
+        attribute=attribute,
+        files=files,
+        update_uncontained=update_uncontained,
     )
--- src/features/document/use_cases/update_document_use_case.py
+++ src/features/document/use_cases/update_document_use_case.py
@@ -4,43 +4,38 @@ from enums import SIMOS
 from fastapi import File, UploadFile
 
 from authentication.models import User
-from restful.request_types.shared import DataSource
 
-from restful.use_case import UseCase
+
 from services.document_service import DocumentService
 from storage.internal.data_source_repository import get_data_source
 
-
-class UpdateDocumentRequest(DataSource):
-    document_id: str
-    data: Union[dict, list]
-    attribute: Optional[str] = None
-    files: Optional[List[UploadFile]] = File(None)
-    update_uncontained: Optional[bool] = True
-
-
-class UpdateDocumentUseCase(UseCase):
-    def __init__(self, user: User, repository_provider=get_data_source):
-        self.user = user
-        self.repository_provider = repository_provider
-
-    def process_request(self, req: UpdateDocumentRequest):
-        document_service = DocumentService(repository_provider=self.repository_provider, user=self.user)
-        if req.attribute and "." in req.document_id:
-            raise ValueError(
-                "Attribute may only be specified in ether dotted path on "
-                + "documentId or the 'attribute' query parameter"
-            )
-        attribute = req.attribute if req.attribute else ""
-        dotted_id = req.document_id + attribute
-        document = document_service.update_document(
-            data_source_id=req.data_source_id,
-            dotted_id=dotted_id,
-            data=req.data,
-            files={f.filename: f.file for f in req.files} if req.files else None,
-            update_uncontained=req.update_uncontained,
+# todo requirements on data_source_id string
+def update_document_use_case(
+    user: User,
+    document_id: str,
+    data_source_id: str,
+    data: Union[dict, list],
+    attribute: Optional[str] = None,
+    files: Optional[List[UploadFile]] = File(None),
+    update_uncontained: Optional[bool] = True,
+    repository_provider=get_data_source,
+):
+    document_service = DocumentService(repository_provider=repository_provider, user=user)
+    if attribute and "." in document_id:
+        raise ValueError(
+            "Attribute may only be specified in ether dotted path on "
+            + "documentId or the 'attribute' query parameter"
         )
-        # Do not invalidate the blueprint cache if it was not a blueprint that was changed
-        if document["data"]["type"] == SIMOS.BLUEPRINT.value:
-            document_service.invalidate_cache()
-        return document
+    attribute = attribute if attribute else ""
+    dotted_id = document_id + attribute
+    document = document_service.update_document(
+        data_source_id=data_source_id,
+        dotted_id=dotted_id,
+        data=data,
+        files={f.filename: f.file for f in files} if files else None,
+        update_uncontained=update_uncontained,
+    )
+    # Do not invalidate the blueprint cache if it was not a blueprint that was changed
+    if document["data"]["type"] == SIMOS.BLUEPRINT.value:
+        document_service.invalidate_cache()
+    return document
