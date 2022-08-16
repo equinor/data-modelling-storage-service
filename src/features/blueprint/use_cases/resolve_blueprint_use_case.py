1991ebdc6e770aa456a575801dd172edbddcc17b
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2022-08-16 12:20:46 +0200

    rename data_source to data_source_id in resolve_blueprint_use_case

--- src/features/blueprint/use_cases/resolve_blueprint_use_case.py
+++ src/features/blueprint/use_cases/resolve_blueprint_use_case.py
@@ -20,18 +20,18 @@ def find_package_with_document(data_source: str, document_id: str, user) -> dict
 
 
 def resolve_blueprint_use_case(user: User, absolute_id: str):
-    data_source, document_id, attr = split_absolute_ref(absolute_id)
+    data_source_id, document_id, attr = split_absolute_ref(absolute_id)
     root_package_found = False
     path_elements = []
-    package = find_package_with_document(data_source, document_id, user)
+    package = find_package_with_document(data_source_id, document_id, user)
     blueprint_name = next((c["name"] for c in package["content"] if c["_id"] == document_id))
     path_elements.append(blueprint_name)
     path_elements.append(package["name"])
     next_document_id = package["_id"]
     while not root_package_found:
-        package = find_package_with_document(data_source, next_document_id, user)
+        package = find_package_with_document(data_source_id, next_document_id, user)
         path_elements.append(package["name"])
         root_package_found = package["isRoot"]
         next_document_id = package["_id"]
     path_elements.reverse()
-    return data_source + "/" + "/".join(path_elements)
+    return data_source_id + "/" + "/".join(path_elements)
