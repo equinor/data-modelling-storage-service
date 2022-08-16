ced8d93fe2945c5d229e238ce41388e24b832968
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2022-08-16 12:20:05 +0200

    fix export_use_case typo

--- src/features/export/export_feature.py
+++ src/features/export/export_feature.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from starlette.responses import FileResponse
 from authentication.authentication import auth_w_jwt_or_pat
 from authentication.models import User
-from .use_cases.export_use_case import export_user_case
+from .use_cases.export_use_case import export_use_case
 
 
 router = APIRouter(tags=["default", "export"], prefix="/export")
@@ -15,7 +15,7 @@ def export(absolute_document_ref: str, user: User = Depends(auth_w_jwt_or_pat)):
     """
     Download a zip-folder of the requested root package
     """
-    memory_file = export_user_case(user=user, document_reference=absolute_document_ref)
+    memory_file = export_use_case(user=user, document_reference=absolute_document_ref)
     response = FileResponse(memory_file, media_type="application/zip")
     response.headers["Content-Disposition"] = "attachment; filename=dmt-export.zip"
 
--- src/features/export/use_cases/export_use_case.py
+++ src/features/export/use_cases/export_use_case.py
@@ -2,6 +2,6 @@ from authentication.models import User
 from services.document_service import DocumentService
 
 
-def export_user_case(user: User, document_reference: str):
+def export_use_case(user: User, document_reference: str):
     memory_file = DocumentService(user=user).create_zip_export(document_reference)
     return memory_file
