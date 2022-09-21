bf5566b5cb26f50e8b6017f6a2b32dce21a97bf7
Kristian Kjerstad <69512295+KristianKjerstad@users.noreply.github.com>
2022-09-21 08:44:34 +0200

    build: use poetry venv inside container (#257)
    To avoid docker build errors.

--- src/init.sh
+++ src/init.sh
@@ -6,15 +6,15 @@ if [ "$1" = 'api' ]; then
     cat version.txt || true
     gunicorn app:create_app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
   else
-    python3 /code/app.py run
+    python3 /code/src/app.py run
   fi
 elif [ "$1" = 'reset-app' ]; then
-  python3 /code/app.py reset-app
+  python3 /code/src/app.py reset-app
 elif [ "$1" = 'create-key' ]; then
-  python3 /code/app.py create-key
+  python3 /code/src/app.py create-key
 elif [ "$1" = 'behave' ]; then
   shift
-  behave "$@"
+  cd src; behave "$@"
 else
   exec "$@"
 fi
