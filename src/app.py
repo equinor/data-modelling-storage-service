b6f726593bef6ec172f9cfa674d15c77fbff8c15
kristian <kristian.kjerstad@bouvet.no>
2022-09-26 08:33:45 +0200

    feature: add logs to inform if CORS turned off

--- src/app.py
+++ src/app.py
@@ -70,6 +70,7 @@ def create_app() -> FastAPI:
     app.include_router(public_routes, prefix=prefix)
 
     if config.ENVIRONMENT == "local":
+        logger.warning("CORS has been turned off. This should only occur in in development.")
         # Turn off CORS when running locally. allow_origins argument can be replaced with a list of URLs.
         app.add_middleware(
             CORSMiddleware,
