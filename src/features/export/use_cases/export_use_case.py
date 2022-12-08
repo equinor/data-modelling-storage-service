471cec292d58dc16b2e6bb9037a9b16efe9c2594
KristianKjerstad <kristian.kjerstad@bouvet.no>
2022-12-08 13:29:52 +0100

    TEMP

--- src/features/export/use_cases/export_use_case.py
+++ src/features/export/use_cases/export_use_case.py
@@ -8,7 +8,7 @@ from domain_classes.tree_node import Node
 from enums import SIMOS
 from services.document_service import DocumentService
 from storage.repositories.zip import ZipFileClient
-
+from features.export.use_cases.export_meta_use_case import _collect_entity_meta_by_path
 
 def create_zip_export(document_service: DocumentService, absolute_document_ref: str, user: User) -> str:
     """Create a temporary folder on the host that contains a zip file.s"""
@@ -25,14 +25,18 @@ def create_zip_export(document_service: DocumentService, absolute_document_ref: 
 
         return archive_path
     if document_node.entity["type"] == SIMOS.PACKAGE.value and not document_node.entity["isRoot"]:
-        # TODO handle non root package
         raise ApplicationException(
             message="Create zip export is only supported for a single document and root package"
         )
 
-    with zipfile.ZipFile(archive_path, mode="w", compression=zipfile.ZIP_DEFLATED, compresslevel=5) as zip_file:
-        # Save the selected node, using custom ZipFile repository
-        document_service.save(document_node, data_source_id, ZipFileClient(zip_file), update_uncontained=True)
+    # with zipfile.ZipFile(archive_path, mode="w", compression=zipfile.ZIP_DEFLATED, compresslevel=5) as zip_file:
+    #     # when exporting a single file, the exported file will inherit meta attribute from its parent
+    #     parent_path = document_path.rsplit("/", 1)[0]
+    #     parent_node: Node = document_service.get_by_path(f"{data_source_id}/{parent_path}")
+    #     if "_meta_" in parent_node.entity:
+    #         document_node.entity["_meta_"] = parent_node.entity["_meta_"] #TODO override is not correct. use _collect_entity_meta_by_path
+    #         # ????
+    #     document_service.save(document_node, data_source_id, ZipFileClient(zip_file), update_uncontained=True)
     return archive_path
 
 
