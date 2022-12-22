77e137dadf084e925cc3e7a9f6ed29d06cfbc988
KristianKjerstad <kristian.kjerstad@bouvet.no>
2022-12-22 12:45:53 +0100

    refactor: fix nested object bug in replace_absolute_references_in_entity_with_alias and add has_dependency_alias checks

--- src/common/utils/replace_reference_with_alias.py
+++ src/common/utils/replace_reference_with_alias.py
@@ -2,17 +2,12 @@ from common.exceptions import ApplicationException
 from domain_classes.dependency import Dependency
 
 
-def is_alias(string_to_check: str, dependencies: list[Dependency]) -> bool:
-    """Check if a given string is an alias or not.
-
-    string_to_check is an alias on the form <alias>:<path> or a document reference on the format <protocol>://<datasource>/<path>
+def has_dependency_alias(reference: str, dependencies: list[Dependency]) -> bool:
+    """Check if a given reference string on the format <protocol>://<datasource>/<path> can be
+    substituted with an alias on the format <alias>:<path>.
     """
     for dependency in dependencies:
-        if (
-            string_to_check.startswith(f"{dependency.alias}:")
-            and dependency.protocol not in string_to_check
-            and dependency.address not in string_to_check
-        ):
+        if reference.startswith(dependency.get_absolute_reference()):
             return True
     return False
 
@@ -33,19 +28,21 @@ def replace_reference_with_alias(reference: str, dependencies: list[Dependency])
 
 
 def replace_absolute_references_in_entity_with_alias(entity: dict, dependencies: list[Dependency]):
-    """Replace path in an entity with alias defined in a list of dependencies.
+    """Replace references in an entity with alias defined in a list of dependencies.
 
     (for example, replace "dmss://system/SIMOS/Package" with "CORE:Package".)
     """
     attributes_to_update = ("type", "attributeType", "_blueprintPath_")  # These keys may contain a reference
-    EXTENDS = "extends"  # extends is a special attribute in an entity that contains a list of referencences
+    EXTENDS = "extends"  # Extends is a special attribute in an entity that contains a list of references
     for attribute in entity:
+        if type(entity[attribute]) == dict:
+            replace_absolute_references_in_entity_with_alias(entity[attribute], dependencies)
         if type(entity[attribute]) == list and attribute != EXTENDS:
             for new_entity in entity[attribute]:
                 replace_absolute_references_in_entity_with_alias(new_entity, dependencies)
-        if attribute in attributes_to_update:
+        if attribute in attributes_to_update and has_dependency_alias(entity[attribute], dependencies):
             entity[attribute] = replace_reference_with_alias(entity[attribute], dependencies)
         elif attribute == EXTENDS:
             for index, reference in enumerate(entity[attribute]):
-                if not is_alias(reference, dependencies):
-                    entity[attribute][index] = replace_reference_with_alias(entity[attribute][index], dependencies)
+                if has_dependency_alias(reference, dependencies):
+                    entity[attribute][index] = replace_reference_with_alias(reference, dependencies)
