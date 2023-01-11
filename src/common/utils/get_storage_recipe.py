7ad961d074957936ed449e48769ef1cc631b6aee
Stig Ofstad <stigofstad@gmail.com>
2023-01-11 11:54:17 +0100

    fix: builtin default ui recipes not used if there is a context

--- src/common/utils/get_storage_recipe.py
+++ src/common/utils/get_storage_recipe.py
@@ -1,19 +1,20 @@
 from common.exceptions import NotFoundException
 from domain_classes.lookup import Lookup
 from domain_classes.storage_recipe import StorageRecipe
+from domain_classes.ui_recipe import Recipe
 from enums import SIMOS
 from storage.internal.lookup_tables import get_lookup
 
-default_ui_recipes = [
-    {
+default_yaml_view = Recipe(
+    **{
         "name": "Yaml",
         "type": SIMOS.UI_RECIPE.value,
         "plugin": "yaml-view",
-        "roles": ["dmss-admin"],
         "category": "view",
-    },
-    {"name": "Edit", "type": SIMOS.UI_RECIPE.value, "plugin": "form", "category": "edit"},
-]
+    }
+)
+default_form_edit = Recipe(**{"name": "Edit", "type": SIMOS.UI_RECIPE.value, "plugin": "form", "category": "edit"})
+default_ui_recipes = [default_form_edit, default_yaml_view]
 
 
 def storage_recipe_provider(type: str, context: str | None = None) -> list[StorageRecipe]:
