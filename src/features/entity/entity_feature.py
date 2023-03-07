d2ec75660a90cf42195126c76cd8addee8430989
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2023-03-07 14:56:36 +0100

    refactor: merge EntityType and Entity

--- src/features/entity/entity_feature.py
+++ src/features/entity/entity_feature.py
@@ -4,9 +4,9 @@ from fastapi.responses import JSONResponse
 from authentication.authentication import auth_w_jwt_or_pat
 from authentication.models import User
 from common.responses import create_response, responses
-from restful.request_types.shared import common_type_constrained_string
+from restful.request_types.shared import Entity, common_type_constrained_string
 
-from .use_cases.instantiate_entity import BasicEntity, instantiate_entity_use_case
+from .use_cases.instantiate_entity import instantiate_entity_use_case
 from .use_cases.validate_entity import validate_entity_use_case
 
 router = APIRouter(tags=["default", "entity"], prefix="/entity")
@@ -14,7 +14,7 @@ router = APIRouter(tags=["default", "entity"], prefix="/entity")
 
 @router.post("", operation_id="instantiate_entity", response_model=dict, responses=responses)
 @create_response(JSONResponse)
-def instantiate(entity: BasicEntity, user: User = Depends(auth_w_jwt_or_pat)):
+def instantiate(entity: Entity, user: User = Depends(auth_w_jwt_or_pat)):
     """Create a new entity and return it.
 
     (entity is not saved in DMSS)
@@ -25,7 +25,7 @@ def instantiate(entity: BasicEntity, user: User = Depends(auth_w_jwt_or_pat)):
 @router.post("/validate", operation_id="validate_entity", responses=responses)
 @create_response(JSONResponse)
 def validate(
-    entity: BasicEntity, as_type: common_type_constrained_string | None = None, user: User = Depends(auth_w_jwt_or_pat)
+    entity: Entity, as_type: common_type_constrained_string | None = None, user: User = Depends(auth_w_jwt_or_pat)
 ):
     """Validate an entity.
     Will return detailed error messages and status code 422 if the entity is invalid.
