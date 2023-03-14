d2c32912f1126690ea096c3b65e015c9d997705f
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2023-03-14 07:23:05 +0100

    fix: improve validate_entity

--- src/features/entity/use_cases/validate_entity.py
+++ src/features/entity/use_cases/validate_entity.py
@@ -1,11 +1,14 @@
 from authentication.models import User
-from common.utils.validators import validate_entity
+from common.utils.validators import validate_entity, validate_entity_against_self
 from restful.request_types.shared import Entity, common_type_constrained_string
 from services.document_service import DocumentService
 
 
 def validate_entity_use_case(entity: Entity, user: User, as_type: common_type_constrained_string | None) -> str:
     document_service = DocumentService(user=user)
-    blueprint = document_service.get_blueprint(as_type if as_type else entity.type)
-    validate_entity(entity.dict(), blueprint, document_service.get_blueprint, allow_extra=bool(as_type))
+    if as_type:
+        validate_entity(
+            entity.dict(), document_service.get_blueprint, document_service.get_blueprint(as_type), "minimum"
+        )
+    validate_entity_against_self(entity.dict(), document_service.get_blueprint)
     return "OK"
