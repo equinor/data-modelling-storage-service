57a5170b9abbb148cfbda7f90b0248cb1431f302
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2023-05-10 16:19:58 +0200

    fix: rewrite traverse_compare to fix holes

--- src/tests/unit/data_structure/traverse_test.py
+++ src/tests/unit/data_structure/traverse_test.py
@@ -17,19 +17,49 @@ class TraverseTestCase(unittest.TestCase):
         actual = [{"key3": "value3"}, {"key2": "value2"}, {"key1": "value1"}]
         expected = [{"key3": "value3"}, {"key2": "value2"}, {"key1": "value-1"}]
         result = traverse_compare(actual, expected)
-        self.assertEqual(result, [{"expected_value": "value-1", "path": "[2].key1", "actual_value": "value1"}])
+        self.assertEqual(
+            result,
+            [
+                {
+                    "expected_value": "value-1",
+                    "path": "[2].key1",
+                    "actual_value": "value1",
+                    "message": "Difference in primitive value",
+                }
+            ],
+        )
 
     def test_compare_lists_missing_entry(self):
         actual = [{"key3": "value3"}, {"key2": "value2"}]
         expected = [{"key3": "value3"}, {"key2": "value2"}, {"key1": "value1"}]
         result = traverse_compare(actual, expected)
-        self.assertEqual(result, [{"expected_value": expected, "path": "", "actual_value": actual}])
+        self.assertEqual(
+            result,
+            [
+                {
+                    "expected_value": expected,
+                    "path": "",
+                    "actual_value": actual,
+                    "message": "Actual length: 2. Expected length: 3",
+                }
+            ],
+        )
 
     def test_compare_lists_extra_entry(self):
         actual = [{"key3": "value3"}, {"key2": "value2"}, {"key1": "value1"}]
         expected = [{"key3": "value3"}, {"key2": "value2"}]
         result = traverse_compare(actual, expected)
-        self.assertEqual(result, [{"expected_value": expected, "path": "", "actual_value": actual}])
+        self.assertEqual(
+            result,
+            [
+                {
+                    "expected_value": expected,
+                    "path": "",
+                    "actual_value": actual,
+                    "message": "Actual length: 3. Expected length: 2",
+                }
+            ],
+        )
 
     def test_compare_dicts_identical(self):
         actual = {"top": {"middle": {"nested": "value"}}}
@@ -41,7 +71,14 @@ class TraverseTestCase(unittest.TestCase):
         expected = {"top": {"middle": {"nested": "wrong value"}}}
         self.assertEqual(
             traverse_compare(actual, expected),
-            [{"expected_value": "wrong value", "path": "top.middle.nested", "actual_value": "value"}],
+            [
+                {
+                    "expected_value": "wrong value",
+                    "path": "top.middle.nested",
+                    "actual_value": "value",
+                    "message": "Difference in primitive value",
+                }
+            ],
         )
 
     def test_compare_missing_key(self):
@@ -54,6 +91,7 @@ class TraverseTestCase(unittest.TestCase):
                     "expected_value": {"nested": "value", "nested2": "value2"},
                     "path": "top.middle",
                     "actual_value": {"nested": "value"},
+                    "message": "Missing keys: nested2. Extra keys: ",
                 }
             ],
         )
@@ -68,6 +106,7 @@ class TraverseTestCase(unittest.TestCase):
                     "expected_value": {"nested": "value"},
                     "path": "top.middle",
                     "actual_value": {"nested": "value", "nested2": "value2"},
+                    "message": "Missing keys: . Extra keys: nested2",
                 }
             ],
         )
