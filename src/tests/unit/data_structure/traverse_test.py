31da9a821c2d1aa839cc258a455bc2923e3d4f28
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2023-05-10 16:18:57 +0200

    test: add missing traverse_compare tests

--- src/tests/unit/data_structure/traverse_test.py
+++ src/tests/unit/data_structure/traverse_test.py
@@ -7,39 +7,67 @@ class TraverseTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_actual_bigger_than_expected(self):
-        actual = [{"key1": "value1"}, {"key2": "value2"}, {"key3": "value3"}]
-        expected = [{"key1": "value1"}, {"key2": "value2"}]
-        result = traverse_compare(actual, expected, ignoreIndexErrors=True, ignoreKeyErrors=True)
-        self.assertEqual(result, [])
-
-    def test_actual_bigger_than_expected_primitives(self):
-        actual = ["a", "b", "c"]
-        expected = ["a", "b"]
-        result = traverse_compare(actual, expected, ignoreIndexErrors=True)
-        self.assertEqual(result, [])
-
-    def test_compare_identical_list_of_dicts(self):
+    def test_compare_lists_identical(self):
         actual = [{"key3": "value3"}, {"key2": "value2"}, {"key1": "value1"}]
         expected = [{"key3": "value3"}, {"key2": "value2"}, {"key1": "value1"}]
         result = traverse_compare(actual, expected)
         self.assertEqual(result, [])
 
-    def test_compare_not_identical_list_of_dicts(self):
+    def test_compare_lists_different_entry(self):
         actual = [{"key3": "value3"}, {"key2": "value2"}, {"key1": "value1"}]
         expected = [{"key3": "value3"}, {"key2": "value2"}, {"key1": "value-1"}]
         result = traverse_compare(actual, expected)
         self.assertEqual(result, [{"expected_value": "value-1", "path": "[2].key1", "actual_value": "value1"}])
 
-    def test_compare_identical_dictionaries(self):
+    def test_compare_lists_missing_entry(self):
+        actual = [{"key3": "value3"}, {"key2": "value2"}]
+        expected = [{"key3": "value3"}, {"key2": "value2"}, {"key1": "value1"}]
+        result = traverse_compare(actual, expected)
+        self.assertEqual(result, [{"expected_value": expected, "path": "", "actual_value": actual}])
+
+    def test_compare_lists_extra_entry(self):
+        actual = [{"key3": "value3"}, {"key2": "value2"}, {"key1": "value1"}]
+        expected = [{"key3": "value3"}, {"key2": "value2"}]
+        result = traverse_compare(actual, expected)
+        self.assertEqual(result, [{"expected_value": expected, "path": "", "actual_value": actual}])
+
+    def test_compare_dicts_identical(self):
         actual = {"top": {"middle": {"nested": "value"}}}
         expected = {"top": {"middle": {"nested": "value"}}}
         self.assertEqual(traverse_compare(actual, expected), [])
 
-    def test_compare_not_identical_dictionaries(self):
+    def test_compare_dicts_different_value(self):
         actual = {"top": {"middle": {"nested": "value"}}}
         expected = {"top": {"middle": {"nested": "wrong value"}}}
         self.assertEqual(
             traverse_compare(actual, expected),
             [{"expected_value": "wrong value", "path": "top.middle.nested", "actual_value": "value"}],
+        )
+
+    def test_compare_missing_key(self):
+        actual = {"top": {"middle": {"nested": "value"}}}
+        expected = {"top": {"middle": {"nested": "value", "nested2": "value2"}}}
+        self.assertEqual(
+            traverse_compare(actual, expected),
+            [
+                {
+                    "expected_value": {"nested": "value", "nested2": "value2"},
+                    "path": "top.middle",
+                    "actual_value": {"nested": "value"},
+                }
+            ],
+        )
+
+    def test_compare_extra_key(self):
+        actual = {"top": {"middle": {"nested": "value", "nested2": "value2"}}}
+        expected = {"top": {"middle": {"nested": "value"}}}
+        self.assertEqual(
+            traverse_compare(actual, expected),
+            [
+                {
+                    "expected_value": {"nested": "value"},
+                    "path": "top.middle",
+                    "actual_value": {"nested": "value", "nested2": "value2"},
+                }
+            ],
         )
