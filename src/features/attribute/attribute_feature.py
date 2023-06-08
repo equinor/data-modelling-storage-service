57a2081707e688be2381f1eb19e4df65570eb465
Eirik Ola Aksnes <eoaksnes@gmail.com>
2023-06-08 13:45:06 +0200

    feat: attribute endpoint

---
+++ src/features/attribute/attribute_feature.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+
+from authentication.authentication import auth_w_jwt_or_pat
+from authentication.models import User
+from common.responses import create_response, responses
+
+from .use_cases.get_attribute_use_case import get_attribute_use_case
+
+router = APIRouter(tags=["default", "attribute"], prefix="/attribute")
+
+
+@router.get(
+    "/{reference:path}",
+    operation_id="attribute_get",
+    response_model=dict,
+    responses=responses,
+)
+@create_response(JSONResponse)
+def get_attribute(reference: str, user: User = Depends(auth_w_jwt_or_pat)):
+    """
+    Fetch the attribute from a reference.
+    """
+    return get_attribute_use_case(user=user, reference=reference)
