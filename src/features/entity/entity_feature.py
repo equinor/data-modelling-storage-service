45a6e04de905418c007d7674e116e593fe175b40
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-07-13 11:01:39 +0200

    feat: add validate existing entity endpoint

--- src/features/entity/entity_feature.py
+++ src/features/entity/entity_feature.py
@@ -8,6 +8,7 @@ from restful.request_types.shared import Entity, common_type_constrained_string
 
 from .use_cases.instantiate_entity import instantiate_entity_use_case
 from .use_cases.validate_entity import validate_entity_use_case
+from .use_cases.validate_existing_entity import validate_existing_entity_use_case
 
 router = APIRouter(tags=["default", "entity"], prefix="/entity")
 
@@ -33,3 +34,13 @@ def validate(
     "as_type": Optional. Validate the root entity against this type instead of the one defined in the entity.
     """
     return validate_entity_use_case(entity=entity, as_type=as_type, user=user)
+
+
+@router.post("/validate-existing-entity/{address:path}", operation_id="validate_existing_entity", responses=responses)
+@create_response(JSONResponse)
+def validate_existing(address: str, user: User = Depends(auth_w_jwt_or_pat)):
+    """Validate an existing entity in dmss.
+    Will return detailed error messages and status code 422 if an entity is invalid.
+
+    """
+    return validate_existing_entity_use_case(address=address, user=user)
---
+++ src/features/entity/use_cases/validate_existing_entity.py
@@ -0,0 +1,15 @@
+from authentication.models import User
+from common.address import Address
+from common.utils.validators import validate_entity_against_self
+from domain_classes.tree_node import Node
+from services.document_service import DocumentService
+
+
+def validate_existing_entity_use_case(address: str, user: User) -> str:
+    document_service = DocumentService(user=user)
+    document_as_node: Node = document_service.get_document(
+        Address.from_absolute(address), depth=999, resolve_links=True
+    )
+    # TODO resolving fails in above line. for some reason the address dmss://DemoDataSource/plugins/DemoDataSource/plugins/grid/blueprints/Dashboard
+    validate_entity_against_self(document_as_node.entity, document_service.get_blueprint)
+    return "OK"
