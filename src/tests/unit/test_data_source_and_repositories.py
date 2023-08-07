c8494aeaeb34993c0b70184b8835d978dd14a0de
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-08-07 15:20:37 +0200

    test: remove test_save_into_multiple_repositories test and add test_save_with_update_uncontained_false instead
    the modeling test_save_into_multiple_repositories is not correct, due to the way model uncontained works.

--- src/tests/unit/test_data_source_and_repositories.py
+++ src/tests/unit/test_data_source_and_repositories.py
@@ -11,20 +11,22 @@ from tests.unit.mock_utils import (
     get_mock_document_service,
     mock_storage_recipe_provider,
 )
-
+from enums import SIMOS
 config.AUTH_ENABLED = False
 test_user = User(**{"user_id": "unit-test", "full_name": "Unit Test", "email": "unit-test@example.com"})
 
 
 class DataSourceTestCase(unittest.TestCase):
-    def test_save_into_multiple_repositories(self):
-        uncontained_doc = {
+    def test_save_with_update_uncontained_false(self):
+        basic_entity = {"_id": "2", "name": "exampleEntity", "description": "", "type": "basic_blueprint"}
+        document = {
             "name": "Parent",
             "description": "",
             "type": "uncontained_blueprint",
-            "uncontained_in_every_way": {"name": "a_reference", "type": "basic_blueprint"},
+            "uncontained_in_every_way": basic_entity # according to blueprint, uncontained_in_every_way is model uncontained.
         }
-
+        # TODO move default_doc_storage, default_get, default_update etc out of tests functions and into the class as
+        # as members to avoid duplicating this part in all tests.
         default_doc_storage = {}
 
         def default_get(document_id: str):
@@ -34,54 +36,38 @@ class DataSourceTestCase(unittest.TestCase):
             default_doc_storage[uid] = data
             return None
 
-        blob_doc_storage = {}
-
-        def blob_get(document_id: str):
-            return blob_doc_storage[document_id]
-
-        def blob_update(uid, data):
-            blob_doc_storage[uid] = data
-            return None
-
         default_repo = mock.Mock()
         default_repo.name = "default"
         default_repo.data_types = [StorageDataTypes.DEFAULT]
         default_repo.get = default_get
         default_repo.update = default_update
-
-        blob_repo = mock.Mock()
-        blob_repo.name = "blob"
-        blob_repo.data_types = [StorageDataTypes.BLOB]
-        blob_repo.get = blob_get
-        blob_repo.update = blob_update
-
         data_source_collection = mock.Mock()
 
         def mock_find_one(**kwargs):
             return None
 
         data_source_collection.find_one = mock_find_one
-
         data_source = DataSource(
             name="testing",
             user=test_user,
-            repositories={"default": default_repo, "blob": blob_repo},
+            repositories={"default": default_repo},
             data_source_collection=data_source_collection,
         )
-
         document_service = get_mock_document_service(lambda x, y: data_source, user=test_user)
-
-        node: Node = tree_node_from_dict(
-            uncontained_doc,
+        uncontained_doc_node: Node = tree_node_from_dict(
+            document,
             uid="1",
             blueprint_provider=document_service.get_blueprint,
             recipe_provider=mock_storage_recipe_provider,
         )
+        document_service.save(uncontained_doc_node, "testing", update_uncontained=True)
 
-        document_service.save(node, "testing", update_uncontained=True)
+        # Test the "uncontained_in_every_way" attribute has been stored as a separate document in the document storage
+        assert default_doc_storage and len(default_doc_storage.keys()) == 2 and default_doc_storage["1"]["uncontained_in_every_way"]["type"] == SIMOS.REFERENCE.value
 
-        # Test that both repos gets written into
-        assert blob_doc_storage and default_doc_storage
+
+
+    # TODO add a test for save in multiple repos (?)
 
     def test_save_based_on_root_storageRecipe(self):
         blob_doc = {"name": "some_entity", "description": "", "type": "blob", "someData": "test"}
