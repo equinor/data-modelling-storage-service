336411cc1de7cdd06949a650b346235c369285b1
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-08-10 09:30:03 +0200

    refactor: move docstring from _get_entity() to CreateEntity class

--- src/common/utils/create_entity.py
+++ src/common/utils/create_entity.py
@@ -23,6 +23,14 @@ class InvalidDefaultValue(CreateEntityException):
 
 
 class CreateEntity:
+    """
+    Rules for creating an entity of a given blueprint type:
+    - all required attributes, as defined in the blueprint, are included.
+      If the required attribute has a default value, that value will be used.
+      If not, an 'empty' value will be used. For example empty string,
+      an empty list, the number 0, etc.
+    - optional attributes value are not included (also true if default value is provided).
+    """
     def __init__(self, blueprint_provider: Callable, type: str):
         if type == BuiltinDataTypes.OBJECT.value:
             type = SIMOS.ENTITY.value
@@ -72,14 +80,6 @@ class CreateEntity:
 
     # type is inserted based on the parent attributes type, or the initial type for root entity.
     def _get_entity(self, blueprint: Blueprint, entity: dict):
-        """
-        Rules for creating an entity of a given blueprint type:
-        - all required attributes, as defined in the blueprint, are included.
-          If the required attribute has a default value, that value will be used.
-          If not, an 'empty' value will be used. For example empty string,
-          an empty list, the number 0, etc.
-        - optional attributes value are not included (also true if default value is provided).
-        """
         for attr in blueprint.attributes:
             if attr.attribute_type == BuiltinDataTypes.BINARY.value:
                 continue
