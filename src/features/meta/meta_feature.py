581fd269484b309354ab17bcd8c4bd584447c253
Peter Bull Hove <peterbullhove@gmail.com>
2023-08-15 14:38:09 +0200

    docs: docstring for meta_feature

--- src/features/meta/meta_feature.py
+++ src/features/meta/meta_feature.py
@@ -19,5 +19,29 @@ router = APIRouter(tags=["default", "meta"], prefix="/meta")
 )
 @create_response(JSONResponse)
 def get_meta_by_id(data_source_id: str, document_id: str, user: User = Depends(auth_w_jwt_or_pat)):
-    """Get meta information from data source id."""
+    """Get Meta Information About a Document. 
+    
+    This endpoint returns meta information about a document provided document id and the id of the data source of which it is located.
+
+    Args: 
+        data_source_id (str): The ID of the data source.
+        document_id (str): The ID of the document. 
+
+    Returns: 
+        dict: A dictionary containing the meta infomation about the document
+            Example: 
+            {
+                "type": "CORE:Meta",
+                "version": "0.0.1",
+                "dependencies": [
+                    {
+                    "type": "dmss://system/SIMOS/Dependency",
+                    "alias": "CORE",
+                    "address": "system/SIMOS",
+                    "version": "0.0.1",
+                    "protocol": "dmss"
+                    }
+                ]
+            }
+    """
     return get_meta_use_case(user=user, data_source_id=data_source_id, document_id=document_id)
