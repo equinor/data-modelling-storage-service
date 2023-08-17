3c742f813c61408b634828a87f4d043304c5f937
Peter Bull Hove <peterbullhove@gmail.com>
2023-08-17 09:50:21 +0200

    refactor: Rename string classes to convention

--- src/features/entity/entity_feature.py
+++ src/features/entity/entity_feature.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 from authentication.authentication import auth_w_jwt_or_pat
 from authentication.models import User
 from common.responses import create_response, responses
-from restful.request_types.shared import Entity, common_type_constrained_string
+from restful.request_types.shared import Entity, TypeConstrainedString
 
 from .use_cases.instantiate_entity import instantiate_entity_use_case
 from .use_cases.validate_entity import validate_entity_use_case
@@ -16,9 +16,17 @@ router = APIRouter(tags=["default", "entity"], prefix="/entity")
 @router.post("", operation_id="instantiate_entity", response_model=dict, responses=responses)
 @create_response(JSONResponse)
 def instantiate(entity: Entity, user: User = Depends(auth_w_jwt_or_pat)):
-    """Create a new entity and return it.
+    """Returns a default entity of specified type. This entity is not stored in the database.
 
-    (entity is not saved in DMSS)
+
+    For storing an entity in the database, see the Post Document endpoint.
+
+    Args:
+    -
+
+    Returns
+    - dict: A JSON dictionary of the specified entity.
+
     Rules for instantiation:
     - all required attributes, as defined in the blueprint, are included.
       If the required attribute has a default value, that value will be used.
@@ -32,7 +40,7 @@ def instantiate(entity: Entity, user: User = Depends(auth_w_jwt_or_pat)):
 @router.post("/validate", operation_id="validate_entity", responses=responses)
 @create_response(JSONResponse)
 def validate(
-    entity: Entity, as_type: common_type_constrained_string | None = None, user: User = Depends(auth_w_jwt_or_pat)
+    entity: Entity, as_type: TypeConstrainedString | None = None, user: User = Depends(auth_w_jwt_or_pat)
 ):
     """Validate an entity.
     Will return detailed error messages and status code 422 if the entity is invalid.
--- src/features/entity/use_cases/validate_entity.py
+++ src/features/entity/use_cases/validate_entity.py
@@ -1,10 +1,10 @@
 from authentication.models import User
 from common.utils.validators import validate_entity, validate_entity_against_self
-from restful.request_types.shared import Entity, common_type_constrained_string
+from restful.request_types.shared import Entity, TypeConstrainedString
 from services.document_service import DocumentService
 
 
-def validate_entity_use_case(entity: Entity, user: User, as_type: common_type_constrained_string | None) -> str:
+def validate_entity_use_case(entity: Entity, user: User, as_type: TypeConstrainedString | None) -> str:
     document_service = DocumentService(user=user)
     if as_type:
         validate_entity(
