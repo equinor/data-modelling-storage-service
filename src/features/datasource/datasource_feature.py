a7dc105ab41853190a2ce48e7381f1a73acc3376
Peter Bull Hove <peterbullhove@gmail.com>
2023-08-18 15:20:23 +0200

    docs: docstrings for datasource_feature.py

--- src/features/datasource/datasource_feature.py
+++ src/features/datasource/datasource_feature.py
@@ -21,7 +21,15 @@ router = APIRouter(tags=["default", "datasource"], prefix="/data-sources")
 @router.get("/{data_source_id}", operation_id="data_source_get", response_model=dict, responses=responses)
 @create_response(JSONResponse)
 def get(data_source_id: str, user: User = Depends(auth_w_jwt_or_pat)):
-    """Get configuration of a single data source."""
+    """Get configuration of a single data source.
+
+    Args:
+    - data_source_id (str): ID of the data source
+    - user (User): The authenticated user accessing the endpoint, automatically generated from provided bearer token or Access-Key.
+
+    Returns:
+    - dict: A dictionary containing configuration for the specified data source.
+    """
     data_source_repository = DataSourceRepository(user)
     return get_data_source_use_case(
         data_source_repository=data_source_repository, data_source=DataSource(data_source_id=data_source_id)
@@ -35,7 +43,21 @@ def save(
     new_data_source: DataSourceRequest,
     user: User = Depends(auth_w_jwt_or_pat),
 ):
-    """Create or update a data source configuration."""
+    """Create or update a data source configuration.
+
+    This endpoint is used for creating or updating a data source configuration.
+    A data source can have multiple repositories.
+
+    Args:
+    - data_source_id (str): ID of the data source
+    - new_data_source (DataSourceRequest): A dict object with keys "name" and "repositories" which is another dict of
+    str and repository configuration. This is the config of the data source.
+    - user (User): The authenticated user accessing the endpoint, automatically generated from provided bearer token or Access-Key.
+
+
+    Returns:
+    - str: The ID of the newly created or updated data source.
+    """
     data_source_repository = DataSourceRepository(user)
     return create_data_source_use_case(
         data_source_id=data_source_id, data_source_repository=data_source_repository, new_data_source=new_data_source
@@ -45,6 +67,13 @@ def save(
 @router.get("", operation_id="data_source_get_all", response_model=list[DataSourceInformation], responses=responses)
 @create_response(JSONResponse)
 def get_all(user: User = Depends(auth_w_jwt_or_pat)):
-    """Get list of all data sources found in DMSS (name and id for each data source)."""
+    """Get list of all data sources found in DMSS.
+
+    Args:
+    - user (User): The authenticated user accessing the endpoint, automatically generated from provided bearer token or Access-Key.
+
+    Returns:
+    - list (DataSourceInformation): A list of information about each data source found in the DMSS protocol.
+    """
     data_source_repository = DataSourceRepository(user)
     return get_data_sources_use_case(data_source_repository=data_source_repository)
