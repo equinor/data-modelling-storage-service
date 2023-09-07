c21003f7ced33a676d7025cde43c77e9758eb684
Peter Bull Hove <peterbullhove@gmail.com>
2023-09-07 12:22:17 +0200

    feat: extend testing coverage of scrypt method to be complete

--- src/tests/unit/test_hashing.py
+++ src/tests/unit/test_hashing.py
@@ -6,8 +6,21 @@ from common.utils.encryption import scrypt
 class HasingTestCase(unittest.TestCase):
     salt = "arandomgeneratedstring"
 
-    def test_scrypt(self):
+    def test_hasing_returns_same_with_same_value_and_same_salt(self):
         value = "My string to hash"
         digest = scrypt(value, self.salt)
         digest2 = scrypt(value, self.salt)
         assert digest == digest2
+
+    def test_hashing_returns_different_with_different_salt_and_same_value(self):
+        value = "My string to hash"
+        digest = scrypt(value, self.salt)
+        digest2 = scrypt(value, "another salt")
+        assert digest != digest2
+
+    def test_hashing_returns_different_with_different_value_and_same_salt(self):
+        value1 = "My string to hash."
+        value2 = "Another string."
+        digest = scrypt(value1, self.salt)
+        digest2 = scrypt(value2, self.salt)
+        assert digest != digest2
