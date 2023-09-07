bfed0dea2ee168abbaac13e165c4cbc30442fe72
Peter Bull Hove <peterbullhove@gmail.com>
2023-09-07 12:07:03 +0200

    feat: error handling in has-key-value-pair

--- src/common/utils/data_structure/has_key_value_pairs.py
+++ src/common/utils/data_structure/has_key_value_pairs.py
@@ -1,9 +1,3 @@
 def has_key_value_pairs(target: dict, content: dict):
     """Util method for checking that target entity contains the key-value pairs defined in content"""
-    is_identical = True
-    for key in content:
-        if key not in target:
-            raise KeyError(f"They key '{key}' does not exist in target {str(target)}")
-        if target[key] != content[key]:
-            is_identical = False
-    return is_identical
+    return all(key in target and target[key] == value for key, value in content.items())
--- src/tests/unit/test_common/test_utils/test_data_structure/test_has_key_value_pairs.py
+++ src/tests/unit/test_common/test_utils/test_data_structure/test_has_key_value_pairs.py
@@ -10,7 +10,14 @@ class HasKeyValuePairsTestCase(unittest.TestCase):
     def test_target_contains_key_value_pair_returns_true(self):
         assert has_key_value_pairs(self.target, {"a": "1"})
         assert has_key_value_pairs(self.target, {"b": "2"})
+        assert has_key_value_pairs(self.target, {"b": "2", "a": "1"})
 
     def test_target_not_contains_key_value_pair_returns_false(self):
         assert has_key_value_pairs(self.target, {"a": "2"}) is False
         assert has_key_value_pairs(self.target, {"b": "1"}) is False
+        assert has_key_value_pairs(self.target, {"a": "1", "b": "1000"}) is False
+
+    def test_target_not_contains_key_returns_false(self):
+        assert has_key_value_pairs(self.target, {"x": "y"}) is False
+        assert has_key_value_pairs(self.target, {1: {"x": "y"}}) is False
+        assert has_key_value_pairs(self.target, {1: {"a": "1"}}) is False
