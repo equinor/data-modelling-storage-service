06e9cbdf29c3736e6d915639ebfde583ba42c564
Peter Bull Hove <peterbullhove@gmail.com>
2023-09-08 13:50:02 +0200

    test: refactored get_blueprint to be less hard-coded

--- src/tests/unit/mock_data/mock_blueprint_provider.py
+++ src/tests/unit/mock_data/mock_blueprint_provider.py
@@ -11,46 +11,25 @@ _FILE_PATH = "src/tests/unit/mock_data/mock_blueprints/"
 class BlueprintProvider:
     @staticmethod
     def get_blueprint(type: str):
-        if type == "all_contained_cases_blueprint":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "basic_blueprint":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "blueprint_with_second_level_reference":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "two_contained_deep_attributes":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "ExtendedBlueprint":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "SecondLevelExtendedBlueprint":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "uncontained_blueprint":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "uncontained_list_blueprint":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "blueprint_with_second_level_nested_uncontained_attribute":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "blueprint_with_optional_attr":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "blueprint_with_nested_optional_attr":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "blob":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "blobContainer":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "blueprint_with_blob":
+        if type in [
+            "BaseChild",
+            "ExtendedBlueprint",
+            "Parent",
+            "SecondLevelExtendedBlueprint",
+            "SpecialChild",
+            "all_contained_cases_blueprint",
+            "basic_blueprint",
+            "blob",
+            "blobContainer",
+            "blueprint_with_blob",
+            "blueprint_with_nested_optional_attr",
+            "blueprint_with_optional_attr",
+            "blueprint_with_second_level_nested_uncontained_attribute",
+            "blueprint_with_second_level_reference",
+            "two_contained_deep_attributes",
+            "uncontained_blueprint",
+            "uncontained_list_blueprint",
+        ]:
             with open(_FILE_PATH + type + ".blueprint.json") as f:
                 return Blueprint(json.load(f), type)
         if type == "test_data/complex/FuelPumpTest":
@@ -73,15 +52,6 @@ class BlueprintProvider:
                 return Blueprint(json.load(f), type)
         if type == "test_data/complex/Customer":
             with open(_FILE_PATH + "Customer.blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "Parent":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "BaseChild":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
-                return Blueprint(json.load(f), type)
-        if type == "SpecialChild":
-            with open(_FILE_PATH + type + ".blueprint.json") as f:
                 return Blueprint(json.load(f), type)
         else:
             return Blueprint(file_repository_test.get(type), type)
