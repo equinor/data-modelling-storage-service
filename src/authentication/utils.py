ceea14a526cea85c0301dcf808352e3fec5595cf
Peter Bull Hove <peterbullhove@gmail.com>
2023-09-29 15:19:21 +0200

    refactor: move into service

--- src/authentication/utils.py
+++ src/authentication/utils.py
@@ -1,20 +1,9 @@
-from typing import Dict, Set
-
-from cachetools import TTLCache, cached
+from typing import Set
 
 from authentication.models import PATData, User
 from common.utils.logging import logger
 from config import config
-from enums import AuthProviderForRoleCheck
-from services.azure_ad_get_app_role_assignments import get_azure_ad_app_role_assignments
-
-
-@cached(cache=TTLCache(maxsize=32, ttl=600))
-def _get_active_roles() -> Dict[str, Set[str]]:
-    match config.AUTH_PROVIDER_FOR_ROLE_CHECK:
-        case AuthProviderForRoleCheck.AZURE_ACTIVE_DIRECTORY:
-            return get_azure_ad_app_role_assignments()
-    return {}
+from services.azure_ad_get_app_role_assignments import get_active_roles
 
 
 def extract_user_from_pat_data(pat_data: PATData) -> User:
@@ -24,6 +13,6 @@ def extract_user_from_pat_data(pat_data: PATData) -> User:
         logger.warn("PAT role assignment validation skipped due to 'TEST_TOKEN=True'")
     else:
         pat_roles: Set[str] = set(pat_data.roles)
-        pat_data.roles = list(pat_roles.intersection(_get_active_roles()[pat_data.user_id]))
+        pat_data.roles = list(pat_roles.intersection(get_active_roles()[pat_data.user_id]))
     user = User(**pat_data.dict())
     return user
