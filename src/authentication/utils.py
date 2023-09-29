ff4d6854328c03425e636c2101daf8e223ab37c5
Peter Bull Hove <peterbullhove@gmail.com>
2023-09-29 15:35:07 +0200

    refactor: move getAR into method again

--- src/authentication/utils.py
+++ src/authentication/utils.py
@@ -3,6 +3,7 @@ from typing import Set
 from authentication.models import PATData
 from common.utils.logging import logger
 from config import config
+from services.azure_ad_get_app_role_assignments import get_active_roles
 
 
 def remove_pat_roles_not_assigned_by_auth_provider(pat_data: PATData, active_roles) -> PATData:
@@ -12,5 +13,5 @@ def remove_pat_roles_not_assigned_by_auth_provider(pat_data: PATData, active_rol
         logger.warn("PAT role assignment validation skipped due to 'TEST_TOKEN=True'")
     else:
         pat_roles: Set[str] = set(pat_data.roles)
-        pat_data.roles = list(pat_roles.intersection(active_roles[pat_data.user_id]))
+        pat_data.roles = list(pat_roles.intersection(get_active_roles()[pat_data.user_id]))
     return pat_data
