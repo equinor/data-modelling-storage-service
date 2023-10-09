e0eb10cba2a737c252718b8ad606ba222a2a6281
Peter Bull Hove <peterbullhove@gmail.com>
2023-10-09 13:56:54 +0200

    test: refactor complex arrays

--- src/tests/unit/services/document_service/test_update_document_complex_arrays.py
+++ src/tests/unit/services/document_service/test_update_document_complex_arrays.py
@@ -1,193 +1,31 @@
 import unittest
 from copy import deepcopy
-from unittest import mock, skip
+from unittest import mock
 
 from authentication.models import User
 from common.address import Address
-from domain_classes.blueprint import Blueprint
-from enums import SIMOS
 from features.document.use_cases.update_document_use_case import (
     update_document_use_case,
 )
-from storage.repositories.file import LocalFileRepository
+from tests.unit.mock_data.mock_blueprint_provider import MockBlueprintProvider
 from tests.unit.mock_data.mock_document_service import get_mock_document_service
-
-package_blueprint = {
-    "type": SIMOS.BLUEPRINT.value,
-    "name": "Package",
-    "description": "This is a blueprint for a package that contains documents and other packages",
-    "extends": [SIMOS.NAMED_ENTITY.value],
-    "attributes": [
-        {"attributeType": "boolean", "type": SIMOS.BLUEPRINT_ATTRIBUTE.value, "name": "isRoot"},
-        {
-            "attributeType": "object",
-            "type": SIMOS.BLUEPRINT_ATTRIBUTE.value,
-            "name": "content",
-            "dimensions": "*",
-            "optional": True,
-        },
-    ],
-}
-
-basic_blueprint = {
-    "type": SIMOS.BLUEPRINT.value,
-    "name": "A box",
-    "description": "First blueprint",
-    "extends": [SIMOS.NAMED_ENTITY.value],
-    "attributes": [
-        {"attributeType": "integer", "type": SIMOS.BLUEPRINT_ATTRIBUTE.value, "name": "length"},
-    ],
-}
-
-higher_rank_array_blueprint = {
-    "type": SIMOS.BLUEPRINT.value,
-    "name": "Higher rank integer arrays",
-    "description": "First blueprint",
-    "extends": [SIMOS.NAMED_ENTITY.value],
-    "attributes": [
-        {
-            "attributeType": "integer",
-            "type": SIMOS.BLUEPRINT_ATTRIBUTE.value,
-            "name": "1_dim-unfixed",
-            "dimensions": "*",
-        },
-        {
-            "attributeType": "basic_blueprint",
-            "type": SIMOS.BLUEPRINT_ATTRIBUTE.value,
-            "name": "1_dim-fixed_complex_type",
-            "dimensions": "5",
-        },
-        {
-            "attributeType": "integer",
-            "type": SIMOS.BLUEPRINT_ATTRIBUTE.value,
-            "name": "2_dim-unfixed",
-            "dimensions": "*,*",
-        },
-        {
-            "attributeType": "integer",
-            "type": SIMOS.BLUEPRINT_ATTRIBUTE.value,
-            "name": "3_dim-mix",
-            "dimensions": "*,1,100",
-        },
-    ],
-}
-
-file_repository_test = LocalFileRepository()
-
-
-class _BlueprintProvider:
-    def get_blueprint(self, template_type: str):
-        if template_type == "higher_rank_array":
-            blueprint = Blueprint(higher_rank_array_blueprint)
-        elif template_type == "package_blueprint":
-            blueprint = Blueprint(package_blueprint)
-        elif template_type == "basic_blueprint":
-            blueprint = Blueprint(basic_blueprint)
-        else:
-            blueprint = Blueprint(file_repository_test.get(template_type))
-        blueprint.path = template_type
-        return blueprint
 
 
 class ArraysDocumentServiceTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        blueprint_provider = _BlueprintProvider()
-        self.mock_document_service = get_mock_document_service(blueprint_provider=blueprint_provider)
-
-    @skip
-    def test_create_complex_array(self):
-        doc_storage = {
-            "1": {
-                "_id": "1",
-                "name": "Package",
-                "description": "My package",
-                "type": SIMOS.PACKAGE.value,
-                "content": [],
-            }
+        simos_blueprints = ["dmss://system/SIMOS/NamedEntity"]
+        mock_blueprint_folder = "src/tests/unit/services/document_service/mock_blueprints/arrays"
+        mock_blueprints_and_file_names = {
+            "basic": "basic.blueprint.json",
+            "package": "package.blueprint.json",
+            "higher_rank_array": "higher_rank_array.blueprint.json",
         }
-
-        def mock_get(document_id: str):
-            return deepcopy(doc_storage[document_id])
-
-        def mock_update(entity: dict, *args, **kwargs):
-            doc_storage[entity["_id"]] = entity
-
-        document_repository = mock.Mock()
-        document_repository.get = mock_get
-        document_repository.update = mock_update
-
-        def repository_provider(data_source_id, user: User):
-            if data_source_id == "testing":
-                return document_repository
-
-        self.mock_document_service.repository_provider = repository_provider
-        self.mock_document_service.add_document(
-            data_source_id="testing",
-            parent_id="1",
-            data={"type": "higher_rank_array", "name": "complexArraysEntity"},
-            attribute_path="content",
+        mock_blueprint_provider = MockBlueprintProvider(
+            mock_blueprints_and_file_names=mock_blueprints_and_file_names,
+            mock_blueprint_folder=mock_blueprint_folder,
+            simos_blueprints_available_for_test=simos_blueprints,
         )
-
-        expected_1 = {"_id": "1", "content": [{"name": "complexArraysEntity", "type": "higher_rank_array"}]}
-        # Disable Black formatting for the matrix
-        # fmt: off
-        expected_2 = {
-            "name": "complexArraysEntity",
-            "type": "higher_rank_array",
-            "1_dim-unfixed": [],
-            "1_dim-fixed_complex_type": [
-                {
-                 "name": "0",
-                 "type": "basic_blueprint",
-                 "description": "",
-                 "length": 0
-                },
-                {
-                    "name": "1",
-                    "type": "basic_blueprint",
-                    "description": "",
-                    "length": 0
-                },
-                {
-                    "name": "2",
-                    "type": "basic_blueprint",
-                    "description": "",
-                    "length": 0
-                },
-                {
-                    "name": "3",
-                    "type": "basic_blueprint",
-                    "description": "",
-                    "length": 0
-                },
-                {
-                    "name": "4",
-                    "type": "basic_blueprint",
-                    "description": "",
-                    "length": 0
-                },
-            ],
-            "2_dim-unfixed": [[]],
-            "3_dim-mix": [
-                [
-                    [
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-                    ]
-                ],
-            ],
-        }
-        # fmt: on
-        self.assertDictEqual(doc_storage["1"], expected_1)
-        self.assertDictEqual(doc_storage[list(doc_storage)[1]], expected_2)
+        self.mock_document_service = get_mock_document_service(blueprint_provider=mock_blueprint_provider)
 
     def test_update_complex_array(self):
         # fmt: off
@@ -200,31 +38,31 @@ class ArraysDocumentServiceTestCase(unittest.TestCase):
                 "1_dim-fixed_complex_type": [
                     {
                         "name": "0",
-                        "type": "basic_blueprint",
+                        "type": "basic",
                         "description": "",
                         "length": 0
                     },
                     {
                         "name": "1",
-                        "type": "basic_blueprint",
+                        "type": "basic",
                         "description": "",
                         "length": 0
                     },
                     {
                         "name": "2",
-                        "type": "basic_blueprint",
+                        "type": "basic",
                         "description": "",
                         "length": 0
                     },
                     {
                         "name": "3",
-                        "type": "basic_blueprint",
+                        "type": "basic",
                         "description": "",
                         "length": 0
                     },
                     {
                         "name": "4",
-                        "type": "basic_blueprint",
+                        "type": "basic",
                         "description": "some description",
                         "length": 0
                     },
@@ -276,31 +114,31 @@ class ArraysDocumentServiceTestCase(unittest.TestCase):
             "1_dim-fixed_complex_type": [
                 {
                     "name": "0",
-                    "type": "basic_blueprint",
+                    "type": "basic",
                     "description": "",
                     "length": 1
                 },
                 {
                     "name": "1",
-                    "type": "basic_blueprint",
+                    "type": "basic",
                     "description": "",
                     "length": 23
                 },
                 {
                     "name": "2",
-                    "type": "basic_blueprint",
+                    "type": "basic",
                     "description": "",
                     "length": 200
                 },
                 {
                     "name": "3",
-                    "type": "basic_blueprint",
+                    "type": "basic",
                     "description": "",
                     "length": 345
                 },
                 {
                     "name": "4",
-                    "type": "basic_blueprint",
+                    "type": "basic",
                     "description": "some other description",
                     "length": 1
                 },
@@ -351,31 +189,31 @@ class ArraysDocumentServiceTestCase(unittest.TestCase):
             "1_dim-fixed_complex_type": [
                     {
                         "name": "0",
-                        "type": "basic_blueprint",
+                        "type": "basic",
                         "description": "",
                         "length": 1
                     },
                     {
                         "name": "1",
-                        "type": "basic_blueprint",
+                        "type": "basic",
                         "description": "",
                         "length": 23
                     },
                     {
                         "name": "2",
-                        "type": "basic_blueprint",
+                        "type": "basic",
                         "description": "",
                         "length": 200
                     },
                     {
                         "name": "3",
-                        "type": "basic_blueprint",
+                        "type": "basic",
                         "description": "",
                         "length": 345
                     },
                     {
                         "name": "4",
-                        "type": "basic_blueprint",
+                        "type": "basic",
                         "description": "some other description",
                         "length": 1
                     },
