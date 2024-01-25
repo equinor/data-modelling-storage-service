c8230aafb9dd971a2ed79a766a74106fa912b01d
Stig Ofstad <stigofstad@gmail.com>
2024-01-25 13:31:41 +0100

    perf: never invalidate blueprint cache

--- src/features/document/use_cases/remove_use_case.py
+++ src/features/document/use_cases/remove_use_case.py
@@ -7,5 +7,4 @@ from storage.internal.data_source_repository import get_data_source
 def remove_use_case(user: User, address: str, repository_provider=get_data_source) -> str:
     document_service = DocumentService(repository_provider=repository_provider, user=user)
     document_service.remove(Address.from_absolute(address))
-    document_service.invalidate_cache()
     return "OK"
