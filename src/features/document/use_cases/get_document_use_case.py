312a7444d2a23cc5780708ac5903a98f89d2fe93
Eirik Ola Aksnes <eoaksnes@gmail.com>
2024-01-25 09:09:35 +0100

    refactor: make resolve document method only return what it is pointing to

--- src/features/document/use_cases/get_document_use_case.py
+++ src/features/document/use_cases/get_document_use_case.py
@@ -2,20 +2,8 @@ from pydantic import conint
 
 from authentication.models import User
 from common.address import Address
-from common.exceptions import BadRequestException
 from services.document_service.document_service import DocumentService
 from storage.internal.data_source_repository import get_data_source
-
-
-def get_nested_dict_attribute(entity: dict | list, path_list: list[str]) -> dict | list:
-    try:
-        if isinstance(entity, list):
-            path_list[0] = int(path_list[0].strip("[]"))  # type: ignore
-        if len(path_list) == 1:
-            return entity[path_list[0]]  # type: ignore
-        return get_nested_dict_attribute(entity[path_list[0]], path_list[1:])  # type: ignore
-    except (KeyError, IndexError) as e:
-        raise BadRequestException(f"Attribute/Item '{path_list[0]}' does not exists in '{entity}'") from e
 
 
 def get_document_use_case(
@@ -26,8 +14,5 @@ def get_document_use_case(
 ):
     """Get document by reference."""
     document_service = DocumentService(repository_provider=repository_provider, user=user)
-    address_object = Address.from_absolute(address)
-    resolved_document, resolved_address = document_service.resolve_document(address_object, depth)
-    if len(resolved_address.attribute_path) > 0:
-        return get_nested_dict_attribute(resolved_document, resolved_address.attribute_path)
+    resolved_document, _ = document_service.resolve_document(Address.from_absolute(address), depth)
     return resolved_document
