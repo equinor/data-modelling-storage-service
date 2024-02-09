26ebaa684255eb9d8823b564c0cee810cd6bc0ca
Peter Bull Hove <peterbullhove@gmail.com>
2024-02-09 12:29:18 +0100

    fix: trying to avoid caching self
    fix: tests
    
    fix: cache

--- src/common/providers/blueprint_provider.py
+++ src/common/providers/blueprint_provider.py
@@ -3,11 +3,12 @@ from collections.abc import Callable
 from functools import lru_cache
 
 from authentication.models import User
-from common.address import Address
-from common.exceptions import NotFoundException
 from common.providers.address_resolver.address_resolver import (
-    ResolvedAddress,
     resolve_address,
+)
+from common.providers.cached_blueprint_provider import (
+    cached_get_blueprint,
+    cached_get_blueprint_with_extended_attributes,
 )
 from common.utils.logging import logger
 from config import config
@@ -27,42 +28,20 @@ class BlueprintProvider:
         self.resolve_address = resolve_address
         self.id = uuid.uuid4()
 
-    @lru_cache(maxsize=config.CACHE_MAX_SIZE)  # noqa: B019
     def get_blueprint_with_extended_attributes(self, type: str) -> Blueprint:
-        blueprint: Blueprint = self.get_blueprint(type)
-        blueprint.realize_extends(self.get_blueprint)
-        logger.debug(f"Cache miss! Fetching extended blueprint '{type}' '{hash(self)} id: {self.id}'")
-        logger.debug(self.get_blueprint_with_extended_attributes.cache_info())
-        return blueprint
+        return cached_get_blueprint_with_extended_attributes(
+            type, self.get_data_source, self.resolve_address, self.user
+        )
 
     @lru_cache(maxsize=config.CACHE_MAX_SIZE)  # noqa: B019
     def get_blueprint(self, type: str) -> Blueprint:
-        logger.debug(f"Cache miss! Fetching blueprint '{type}' '{hash(self)}'")
-        try:
-            resolved_address: ResolvedAddress = self.resolve_address(
-                Address.from_absolute(type),
-                lambda data_source_name: self.get_data_source(data_source_name, self.user),
-            )
-        except NotFoundException as ex:
-            raise NotFoundException(
-                f"Blueprint referenced with '{type}' could not be found. Make sure the reference is correct.",
-                data=ex.dict(),
-            ) from ex
-        resolved_address = self.resolve_address(
-            Address.from_relative(
-                resolved_address.entity["address"],
-                resolved_address.document_id,
-                resolved_address.data_source_id,
-            ),
-            lambda data_source_name: self.get_data_source(data_source_name, self.user),
-        )
-        return Blueprint(resolved_address.entity, type)
+        return cached_get_blueprint(type, self.get_data_source, self.resolve_address, self.user)
 
     def invalidate_cache(self):
         try:
             logger.debug("invalidate cache")
-            self.get_blueprint.cache_clear()
-            self.get_blueprint_with_extended_attributes.cache_clear()
+            cached_get_blueprint_with_extended_attributes.cache_clear()
+            cached_get_blueprint.cache_clear()
         except Exception as error:
             logger.warning("function is not instance of lru cache.", error)
 
@@ -76,6 +55,3 @@ def get_blueprint_provider():
     return BlueprintProvider(
         User(**{"user_id": config.DMSS_ADMIN, "email": "mock_dmss_admin@example.com", "full_name": "Mock admin user"})
     )
-
-
-default_blueprint_provider = get_blueprint_provider()
