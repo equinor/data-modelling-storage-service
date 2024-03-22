7d71cbf8963e229b84639160aef5787cbb657e68
Stig Ofstad <stigofstad@gmail.com>
2024-03-22 14:35:21 +0100

    fix: no longer use gunicorn to scale workers

--- src/init.sh
+++ src/init.sh
@@ -17,12 +17,8 @@ if [ "$1" = 'api' ]; then
     python3 /code/src/app.py reset-app
   fi
 
-  if [ "${ENVIRONMENT:-'local'}" != "local" ]; then
-    cat version.txt || true
-    gunicorn app:create_app --workers 4 --disable-redirect-access-to-syslog --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000 --graceful-timeout 180 --timeout 180
-  else
-    python3 /code/src/app.py run
-  fi
+  python3 /code/src/app.py run
+
 elif [ "$1" = 'reset-app' ]; then
   python3 /code/src/app.py reset-app
 elif [ "$1" = 'create-key' ]; then
